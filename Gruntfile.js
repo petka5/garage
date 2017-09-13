2e2a1f05c5156d5b54e7f7183896afdd53b49816
petka <petar.georgiev@gmail.com>
2017-09-13 21:27:36 +0300

    Added grunt task to start mongod.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,18 @@
-'use strict';
+//'use strict';
 
 module.exports = function(grunt) {
 
 	grunt.initConfig({
 		pkg : grunt.file.readJSON('package.json'),
+		run : {
+			mongo : {
+				options : {
+					cwd : 'C:/Development/mongodb/mongodb-win32-x86_64-2008plus-ssl-3.0.3-rc2/bin'
+				},
+				cmd : 'mongod',
+				args : [ '--config=../../mongod.conf' ]
+			}
+		},
 		copy : {
 			files : {
 				expand : true,
@@ -21,6 +30,8 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-
+	grunt.loadNpmTasks('grunt-run');
+	
+	grunt.registerTask("start", [ "run:mongo" ]);
 	grunt.registerTask("default", [ "watch" ]);
 }
