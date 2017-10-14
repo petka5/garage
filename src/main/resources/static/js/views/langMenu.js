1285d612faf53cbfb028d95574a9fae4035ee9fc
petka <petar.georgiev@gmail.com>
2017-10-14 12:10:20 +0300

    Commit langMenu changes.

--- src/main/resources/static/js/views/langMenu.js
+++ src/main/resources/static/js/views/langMenu.js
@@ -3,42 +3,42 @@ define([ 'jquery', 'underscore', 'backbone', 'marionette', 'backbone-validation'
 
 // function
 function($, _, Backbone, Marionette, Validator, i18n, LanguageCollection, Language, langMenuTemplate) {
-	var LangMenuView = Marionette.View.extend({
-		template : _.template(langMenuTemplate),
-		events : {
-			'click .lang' : 'changeLanguage'
+	
+	// http://jasonwatmore.com/post/2015/03/10/angularjs-user-registration-and-login-example-tutorial
+	// http://jonathancreamer.com/require-js-packages-for-building-large-scale-angular-applications/
+	// https://www.startersquad.com/blog/angularjs-requirejs/
+	var LangMenuView = Marionette.View.extend({   
+		template : _.template('<% _.each(languages, function(lang) { %>\
+				<span class="lang" data-lang="<%= lang.lang %>"><%= lang.lang %></span>\
+				<% });%>'), 
+		
+		events : { 
+			'click .lang' :'changeLanguage'
 		},
-
+ 
 		changeLanguage : function(ev) {
 			i18n.setLng($(ev.target).attr('data-lang'), function(){
 				$('body').i18n();
 			});
 		},
-
+		
 		initialize : function() {
-			var self = this;
+			console.log('initialize');
 			this.languages = new LanguageCollection();
-			this.languages.fetch({
-				success : function() {
-					self.render();
-				}
-			});
+			this.listenTo(this.languages, 'sync', this.addLang);
+			this.languages.fetch();
 		},
-
-		render : function() {
+		
+		addLang: function (){
 			var compiledTemplate = _.template(this.template({
-				languages : this.languages.toJSON()
-			}));
-			// Append our compiled template to this Views "el"
-			this.$el.append(compiledTemplate);
+				languages : this.languages.toJSON() 
+			})); 
+			this.$el.append(compiledTemplate); 
 		},
-
-		remove : function() {
-			this.$el.empty().off();
-			return Backbone.View.prototype.remove.apply(this, arguments);
-			// off to unbind the events
-			this.stopListening();
+		render : function() { 
+			console.log('render');
 		}
+ 
 	});
 	// Our module now returns our view
 	return LangMenuView;
